163dbb8bea937576557c0730413be2b81c0a3554
Tom Shmueli <tom.shmueli@personetics.com>
2025-09-14 14:53:05 +0300

    fix requirement problem

--- Checklist/settings.py
+++ Checklist/settings.py
@@ -30,7 +30,7 @@ R_AND_D_THRESHOLD_LOW = 0.15
 # Example file path for the Google Sheet
 EXCEL_NAME = "my_Portfolio_Dashboard"
 PORTFOLIO_SHEET_NAME = "Portfolio"  # The sheet responsible for storing the tickers
-FEROLDI_SHEET_NAME = "Feroldi Quality Score" # The sheet responsible for storing the Feroldi Scores / checklist
+FEROLDI_SHEET_NAME = "Scores" # The sheet responsible for storing the Feroldi Scores / checklist
 START_COL = 4  # Column index where tickers start in the sheet
 FINANCIAL_ROWS = {
     "Resilience Score": 5,     # Row for Resilience Score
